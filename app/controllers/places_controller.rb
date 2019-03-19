1302dc4c0298b674bcbc5e193f02f4e2a17dacba
xandrix ecklor <xandrixecklor@hotmail.com>
2019-03-19 22:07:02 +0000

    Initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def index
 		# .pagiinate(page: params[:page], per_page: 1 "change how many you want each page to show")
@@ -11,8 +11,12 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
 
 	def show
@@ -21,16 +25,32 @@ class PlacesController < ApplicationController
 
 	def edit
 			@place = Place.find(params[:id])
+
+			if @place.user != current_user
+				return render plain: 'Not Allowed', status: :forbidden
+			end
 	end
 
 	def update
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render plain: 'Not Allowed', status: :forbidden
+		end
+
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid
+			redirect_to root_path
+		else
+			render :edit, status: :unprocessable_entity
+		end
 	end
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render plain: 'Not Allowed', status: :forbidden
+		end
+
 		@place.destroy
 		redirect_to root_path
 	end
