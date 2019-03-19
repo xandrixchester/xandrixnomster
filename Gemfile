4ba7c35e52c4634299f14659d1a29d6180566298
xandrix ecklor <xandrixecklor@hotmail.com>
2019-03-19 14:38:46 +0000

    Initial commit

--- Gemfile
+++ Gemfile
@@ -66,6 +66,12 @@ gem 'jquery-rails'
 gem 'popper_js', '~> 1.11.1'
 gem 'bootstrap', '4.0.0.alpha6'
 
+gem 'will_paginate', '>= 3.1'
+
+gem 'simple_form'
+
+gem "font-awesome-rails"
+
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,24 @@
 class PlacesController < ApplicationController
 
 	def index
-		@places = Place.all
+		# .pagiinate(page: params[:page], per_page: 1 "change how many you want each page to show")
+		@places = Place.all.paginate(page: params[:page], per_page: 1)
 	end
+
+	def new
+		@place = Place.new
+	end
+
+	def create
+		Place.create(place_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:name, :description, :address)
+	end
+
+
 end
