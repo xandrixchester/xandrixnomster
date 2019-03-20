82df19cd09bbfd63a01287c75bf2d8f182d885be
xandrix ecklor <xandrixecklor@hotmail.com>
2019-03-20 18:22:31 +0000

    Initial commit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,12 @@ class Comment < ApplicationRecord
 	after_validation :geocode 
 
 	validates :name, presence: true
+
+	RATINGS = {
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
+  }
 end
